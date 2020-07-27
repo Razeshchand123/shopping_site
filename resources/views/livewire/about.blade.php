bd055f8510d00b87f809f9b372a1d5d3520e013f
Razeshchand <razesh84chand@gmail.com>
2020-07-27 03:50:40 -0700

    testomonial added

--- resources/views/livewire/about.blade.php
+++ resources/views/livewire/about.blade.php
@@ -10,7 +10,7 @@
   <p>
  
 <span class="display-4 tcolor">
-<img src=" storage/thispics/logo.png" alt=""  class="border border-dark img-responsive rounded-circle p-2" width="50" height="50">
+<img src=" storage/thispics/logo.png" alt="no img"  class="border border-dark img-responsive rounded-circle p-2" width="50" height="50"/>
  
 Digital Bazar
 
@@ -108,44 +108,15 @@ Digital Bazar
 <!-- 2nd part end  -->
 
 
-<!-- 3rd map part start  -->
-<div class="row mt-5">
-
-<div class="col-sm-8 m-auto" style="box-shadow: 1px 2px 14px 4px #540246;
-    padding: 2%;">
-<h3>  हामी लाई सम्पर्क गर्नुस् | </h3>
-<hr>
-
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1424.9742127794807!2d80.17676488998691!3d28.96229427280963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39a1ac07c88b98ad%3A0xc3874da199ecddb1!2sMahendranagar%20Bazaar%2C%20Bhimdatta%2010400!5e1!3m2!1sen!2snp!4v1595843863719!5m2!1sen!2snp" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-
-
-
-
-
-
-
-
-
-
-
-</div>
-
-</div>
-
-<!-- 3rd map part end here  -->
 
 
 <!-- testomonial part  -->
 <div class="row mt-5">
 
-<div class="col-sm-8 m-auto" style="box-shadow: 1px 2px 14px 4px #540246;
+<div class="col-sm-8 m-auto bgg" style="box-shadow: 1px 2px 14px 4px #540246;
     padding: 2%;">
 
-  
 
-<h3>  हामी लाई सम्पर्क गर्नुस् | </h3>
-<hr>
 
 <!-- carosul start  -->
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -154,22 +125,44 @@ Digital Bazar
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
-  <div class="carousel-inner">
+  <div class="carousel-inner bgg">
     <div class="carousel-item active">
       <div class="d-block w-100" >
-      <i >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure quos illo totam nemo tempora illum quod nihil deleniti. Quaerat expedita obcaecati error autem praesentium inventore tempora fugiat accusamus non suscipit!</i>
+
+
+      <div class="d-flex">
+      <img src="storage/testo/one.jpeg" alt="no img"  class=" text-center border border-dark img-responsive p-2 rounded-circle"height="100" width="100"/>
+      <i class="m-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure quos illo totam nemo tempora illum quod nihil deleniti. Quaerat expedita obcaecati error autem praesentium inventore tempora fugiat accusamus non suscipit!</i>
+      </div>
+
+       </div>
+    </div>
+    <div class="carousel-item">
+      <div class="d-block w-100" >
       
+
+
+      <div class="d-flex">
+      <img src="storage/testo/three.jpeg" alt="no img"  class=" text-center border border-dark img-responsive p-2 rounded-circle"height="100" width="100"/>
+      <i class="m-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure quos illo totam nemo tempora illum quod nihil deleniti. Quaerat expedita obcaecati error autem praesentium inventore tempora fugiat accusamus non suscipit!</i>
       </div>
+
+
+
+         </div>
     </div>
     <div class="carousel-item">
       <div class="d-block w-100" >
-      <i >Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, recusandae eos. Aspernatur quia neque similique tenetur voluptatem perspiciatis nemo enim autem nobis ipsam quas vel sequi ad cupiditate, animi repellendus.</i>
+     
+         
+         
+      <div class="d-flex">
+      <img src="storage/testo/two.jpeg" alt="no img"  class=" text-center border border-dark img-responsive p-2 rounded-circle"height="100" width="100"/>
+      <i class="m-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure quos illo totam nemo tempora illum quod nihil deleniti. Quaerat expedita obcaecati error autem praesentium inventore tempora fugiat accusamus non suscipit!</i>
       </div>
-    </div>
-    <div class="carousel-item">
-      <div class="d-block w-100" >
-      <i >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis a doloremque quae possimus officiis, soluta alias, cum voluptatibus autem, tempora saepe voluptates earum dolores ratione ea corrupti delectus esse odio!</i>
-      </div>
+
+
+       </div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -196,6 +189,33 @@ Digital Bazar
 </div>
 
 <!-- testmonial part  -->
+
+
+ 
+
+
+
+
+<!-- 3rd map part start  -->
+<div class="row mt-5">
+
+<div class="col-sm-8 m-auto" style="box-shadow: 1px 2px 14px 4px #540246;
+    padding: 2%;">
+<h3>  हामी लाई सम्पर्क गर्नुस् | </h3>
+<hr>
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1424.9742127794807!2d80.17676488998691!3d28.96229427280963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39a1ac07c88b98ad%3A0xc3874da199ecddb1!2sMahendranagar%20Bazaar%2C%20Bhimdatta%2010400!5e1!3m2!1sen!2snp!4v1595843863719!5m2!1sen!2snp" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+
+</div>
+
+</div>
+
+<!-- 3rd map part end here  -->
+
+
+
 
 
 
